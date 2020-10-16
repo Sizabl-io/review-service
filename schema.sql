c96c32043c3b740d2aea51c4e8bb942621253899
codingfox <carinalxx@gmail.com>
2020-10-16 16:04:42 -0700

    update schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE  IF EXISTS reviews_database;
+DROP DATABASE IF EXISTS reviews_database;
 CREATE DATABASE reviews_database;
 
 DROP TABLE IF EXISTS reviews;
@@ -6,25 +6,25 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS restaurants;
 
 -- \c reviews_database
-CREATE TABLE IF NOT EXISTS "restaurants" (
+CREATE TABLE "restaurants" (
   "restaurant_id" SERIAL PRIMARY KEY,
   "restaurant_name" VARCHAR(60) UNIQUE NOT NULL,
   "number_of_reviews" INT,
-  "overall_rating" DECIMAL,
+  "overall_rating" DECIMAL
 );
 
 
-CREATE TABLE IF NOT EXISTS  "users" (
+CREATE TABLE "users" (
   "user_id" SERIAL PRIMARY KEY,
   "first_name" VARCHAR(60) NOT NULL,
   "last_name" VARCHAR(60) NOT NULL,
   "avatar" INT,
-  "location" VARCHAR(200)，
+  "location" VARCHAR(200),
   "number_of_reviews" INT
 );
 
 
-CREATE TABLE IF NOT EXISTS "reviews" (
+CREATE TABLE "reviews" (
   "review_id" SERIAL PRIMARY KEY,
   "restaurant_id" INT,
   "user_id" INT,
