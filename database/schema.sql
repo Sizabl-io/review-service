924b5856b8db7824b66473b01267cc75d6df8803
codingfox <carinalxx@gmail.com>
2020-10-15 20:45:30 -0700

    cassandra schema completed

---
+++ database/schema.sql
@@ -0,0 +1,40 @@
+DROP KEYSPACE IF EXISTS "reviewservice";
+
+CREATE KEYSPACE "reviewservice" WITH REPLICATION = {
+   'class' : 'SimpleStrategy',
+   'replication_factor' : 1
+  };
+
+  USE "reviewservice";
+
+  CREATE TABLE user (
+    user_id int,
+    first_name text,
+    last_name text,
+    avatar text,
+    location text,
+    PRIMARY KEY (user_id)
+);
+
+  CREATE TABLE "restaurants" (
+    restaurant_id int,
+    restaurant_name text,
+    number_of_reviews int,
+    overall_rating int,
+    one_star_rating int,
+    two_star_rating int,
+    three_star_rating int,
+    four_star_rating int,
+    five_star_rating int,
+    PRIMARY KEY ((restaurant_id, id), date, user_id)
+  )
+  WITH CLUSTERING ORDER BY (overall_rating, number_of_reviews)
+
+  CREATE TABLE "reviews" (
+    review_id int,
+    restaurant_id int,
+    user_id int,
+    create_date date,
+    description text,
+    rating int
+  )
