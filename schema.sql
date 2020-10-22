fb70d75c48835d96b4ef851b48243204234552b5
codingfox <carinalxx@gmail.com>
2020-10-22 13:19:52 -0700

    completed database

--- schema.sql
+++ schema.sql
@@ -15,9 +15,10 @@ CREATE TABLE "restaurants" (
 
 CREATE TABLE "users" (
   "user_id" SERIAL PRIMARY KEY,
-  "user_name" VARCHAR(60) NOT NULL,
-  "avatar" VARCHAR,
-  "user_location" VARCHAR(200),
+  "first_name" VARCHAR(60) NOT NULL,
+  "last_name" VARCHAR(60) NOT NULL,
+  "avatar" INT,
+  "location" VARCHAR(200),
   "number_of_reviews" INT
 );
 
